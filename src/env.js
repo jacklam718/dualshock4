df7b9d7bcfc8dc0c5efd071aeded39cb9e7462b3
jacklam718 <jacklam718@gmail.com>
2019-03-29 05:34:27 +0800

    Fix for condition for `isIphoneX`

--- src/env.js
+++ src/env.js
@@ -3,4 +3,4 @@ import { Dimensions, Platform } from 'react-native';
 export const deviceWidth = Dimensions.get('window').width;
 export const deviceHeight = Dimensions.get('window').height;
 export const isIOS = Platform.OS === 'ios';
-export const isIphoneX = Platform.OS === isIOS && (deviceWidth === 812 || deviceHeight === 812);
+export const isIphoneX = isIOS && (deviceWidth === 812 || deviceHeight === 812);
